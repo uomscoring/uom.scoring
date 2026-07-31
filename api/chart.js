d5bcaf519c0de7cb14d0c0b61bc0c34ab535dfa5
uomscoring <universeofmusic2026@gmail.com>
2026-08-01 05:02:14 +0800

    Add handler to fetch and return CSV data

--- api/chart.js
+++ api/chart.js
@@ -1,1 +1,10 @@
-
+    export default async function handler(req, res) {
+      const SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT2ZX1gsf_L1_ioUZLmBVEwCIolt92WicQKJkoM0nn3p7A2GrBfoiQMZVOHvhXJ4ISBkK_pWzmvntyx/pub?gid=1352500872&single=true&output=csv';
+      
+      const response = await fetch(SHEET_URL);
+      const text = await response.text();
+      
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
+      res.status(200).send(text);
+    }
